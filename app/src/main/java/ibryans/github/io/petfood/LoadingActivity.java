d9188c4a85d7062fd4c3ed9b0046aaa78454071b
ibryans <bryans0701@gmail.com>
2018-09-14 15:30:59 -0300

    Tela de carregamento

---
+++ app/src/main/java/ibryans/github/io/petfood/LoadingActivity.java
@@ -0,0 +1,27 @@
+package ibryans.github.io.petfood;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+public class LoadingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+
+        ProgressBar progress = findViewById(R.id.progress);
+        ImageView img = findViewById(R.id.logo);
+
+    }
+}
