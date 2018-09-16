626b86b5a4d71d6a0f354b48edb00c3d0cb58e81
ibryans <bryans0701@gmail.com>
2018-09-16 12:41:34 -0300

    Tela de abertura finalizada

--- app/src/main/java/ibryans/github/io/petfood/LoadingActivity.java
+++ app/src/main/java/ibryans/github/io/petfood/LoadingActivity.java
@@ -3,7 +3,9 @@ package ibryans.github.io.petfood;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -13,15 +15,35 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
+import static ibryans.github.io.petfood.R.animator.fade_in;
+
 public class LoadingActivity extends Activity {
 
+    Animation fadeIn;
+
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
 
-        ProgressBar progress = findViewById(R.id.progress);
-        ImageView img = findViewById(R.id.logo);
+        final ImageView logo = findViewById(R.id.logo);
+        Glide.with(this).load(R.drawable.logo).into(logo);
 
+        fadeIn = AnimationUtils.loadAnimation(getApplicationContext(), fade_in);
+
+        Handler handle = new Handler();
+
+        final Intent main = new Intent(LoadingActivity.this, MainActivity.class);
+
+        logo.startAnimation(fadeIn);
+
+        handle.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+                startActivity(main);
+            }
+        }, 1500);
     }
 }
